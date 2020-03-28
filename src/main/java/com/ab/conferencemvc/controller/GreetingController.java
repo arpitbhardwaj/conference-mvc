78023c95c936c65dd3f0a479df39649001346a0f
Arpit Bhardwaj <arpit.bhardwaj@sap.com>
2020-03-28 17:10:48 +0530

    Added Greeting Controller

---
+++ src/main/java/com/ab/conferencemvc/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package com.ab.conferencemvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+/**
+ * @author Arpit Bhardwaj
+ */
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("greeting")
+    public String greeting (Map<String,Object> model){
+        model.put("message","Hello Arpit");
+        return "greeting";
+    }
+}
